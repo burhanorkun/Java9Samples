591f791bb77222afa0828aec3fef0cf5f22ee23c
burhanorkun <beyazmartilar@gmail.com>
2016-11-15 12:28:59 +0200

    first commit

---
+++ TestPrinterModule/src/com/printer/util/Util.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.printer.util;
+
+/**
+ *
+ * @author TCBORKUN
+ */
+public class Util {
+    
+    public void yazdir(String message){
+        System.out.println(message);
+    }
+}
